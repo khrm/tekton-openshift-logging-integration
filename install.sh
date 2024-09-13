beb7e94de2ac6bf4a45762b072c59892b954e63f
Khurram Baig <kbaig@redhat.com>
2024-09-13 15:23:39 +0530

    Add install script for Loki

---
+++ install.sh
@@ -0,0 +1,9 @@
+oc create -f minio.yaml
+oc create -f minio-svc.yaml
+oc expose svc/minio
+minioURL=$(oc get route minio  --no-headers -o custom-columns=":spec.host")
+mc alias set myminio http://${minioURL} minioadmin minioadmin --insecure
+mc mb myminio/loki
+oc create secret generic logging-loki-minio   --from-literal=bucketnames="loki"   --from-literal=endpoint="http://${minioURL}"  --from-literal=access_key_id="minioadmin"   --from-literal=access_key_secret="minioadmin" -n openshift-logging
+oc create -f lokistack-gcp.yaml
+oc create -f logging-gcp.yaml
