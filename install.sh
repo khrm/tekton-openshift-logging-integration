8f17c71d279545f7f40bfc4fc985184bb4208b09
Khurram Baig <kbaig@redhat.com>
2024-09-18 14:10:38 +0530

    Add check for lokistack and logging operator and sc automation
    Add check for lokistack and logging operator.
    Storage class automation and choose default sc for lokistack.yaml

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+if [ $(oc get crd | grep lokistack | wc -l) -lt 1 ]; then
+    echo "Please Install LokiStack Operator or wait for installation to finish"
+fi
+
+if [ $(oc get crd | grep clusterlogforwarder | wc -l) -lt 1 ]; then
+    echo "Please Install OpenShift Logging Operator or wait for installation to finish"
+fi
+
+
 oc create -f minio.yaml
 oc create -f minio-svc.yaml
 oc expose svc/minio
@@ -6,6 +15,7 @@ minioURL=$(oc get route minio  --no-headers -o custom-columns=":spec.host")
 mc alias set myminio http://${minioURL} minioadmin minioadmin --insecure
 mc mb myminio/loki
 oc create secret generic logging-loki-minio   --from-literal=bucketnames="loki"   --from-literal=endpoint="http://${minioURL}"  --from-literal=access_key_id="minioadmin"   --from-literal=access_key_secret="minioadmin" -n openshift-logging
-oc create -f lokistack-gcp.yaml
-oc create -f logging-gcp.yaml
+sed -i "s/storageClassName.*/storageClassName: $(kubectl get storageclass | grep default | awk '{print $1}')/g" lokistack.yaml
+oc create -f lokistack.yaml
+oc create -f logging.yaml
 oc create -f clusterlogforwarder.yaml
